c3bd87dedce1810eedefb26189b95aa9288e2a84
Joey Huab <joey@evolution-x.org>
2020-01-11 20:59:25 +0000

    raphael: Use Coral fingerprint from Jan 2020 sec patch

--- init/init_raphael.cpp
+++ init/init_raphael.cpp
@@ -93,7 +93,8 @@ void vendor_load_properties() {
     }
 
     property_override("ro.bluetooth.a2dp_offload.supported", "false");
-    property_override("ro.build.fingerprint", "google/walleye/walleye:8.1.0/OPM1.171019.011/4448085:user/release-keys");
+    property_override("ro.vendor.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
+    property_override("ro.build.fingerprint", "google/coral/coral:10/QQ1B.200105.004/6031802:user/release-keys");
     property_override("ro.control_privapp_permissions", "log");
     property_override("persist.bluetooth.a2dp_offload.disabled", "true");
     property_override("persist.bluetooth.bluetooth_audio_hal.disabled", "true");
