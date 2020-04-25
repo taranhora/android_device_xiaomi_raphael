bc1aee41463baf99130063e8ffc53c278a675c64
TheScarastic <warabhishek@gmail.com>
2020-04-25 09:10:32 +0530

    raphael: parts: Refactor whole code

--- parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
+++ parts/src/org/lineageos/settings/popupcamera/PopupCameraService.java
@@ -25,15 +25,12 @@ import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
 import android.hardware.SensorManager;
-import android.os.IBinder;
 import android.os.Handler;
-import android.os.SystemClock;
-import android.os.UserHandle;
+import android.os.IBinder;
 import android.util.Log;
 
-import java.util.List;
-
 import org.lineageos.internal.util.FileUtils;
+
 import vendor.xiaomi.hardware.motor.V1_0.IMotor;
 
 public class PopupCameraService extends Service {
@@ -42,27 +39,47 @@ public class PopupCameraService extends Service {
     private static final boolean DEBUG = false;
     private static final String closeCameraState = "0";
     private static final String openCameraState = "1";
-    private static String mCameraState = "-1";
-
-    private IMotor mMotor = null;
-
-    private SensorManager mSensorManager;
-    private Sensor mFreeFallSensor;
     private static final int FREE_FALL_SENSOR_ID = 33171042;
-
     private static final String GREEN_LED_PATH = "/sys/class/leds/green/brightness";
     private static final String BLUE_LED_PATH = "/sys/class/leds/blue/brightness";
-
+    private static String mCameraState = "-1";
     private static Handler mHandler = new Handler();
+    private IMotor mMotor = null;
+    private SensorManager mSensorManager;
+    private Sensor mFreeFallSensor;
+    private SensorEventListener mFreeFallListener = new SensorEventListener() {
+        @Override
+        public void onSensorChanged(SensorEvent event) {
+            if (event.sensor.getType() == FREE_FALL_SENSOR_ID && event.values[0] == 2.0f) {
+                updateMotor(closeCameraState);
+                goBackHome();
+            }
+        }
+
+        @Override
+        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        }
+    };
+    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            final String action = intent.getAction();
+            if (lineageos.content.Intent.ACTION_CAMERA_STATUS_CHANGED.equals(action)) {
+                mCameraState = intent.getExtras().getString(lineageos.content.Intent.EXTRA_CAMERA_STATE);
+                updateMotor(mCameraState);
+            }
+        }
+    };
 
     @Override
     public void onCreate() {
-        mSensorManager = this.getSystemService(SensorManager.class);
+        mSensorManager = getSystemService(SensorManager.class);
         mFreeFallSensor = mSensorManager.getDefaultSensor(FREE_FALL_SENSOR_ID);
         registerReceiver();
         try {
             mMotor = IMotor.getService();
-        } catch(Exception e) {
+        } catch (Exception e) {
+            // Do nothing
         }
     }
 
@@ -75,7 +92,7 @@ public class PopupCameraService extends Service {
     @Override
     public void onDestroy() {
         if (DEBUG) Log.d(TAG, "Destroying service");
-        this.unregisterReceiver(mIntentReceiver);
+        unregisterReceiver(mIntentReceiver);
         super.onDestroy();
     }
 
@@ -86,38 +103,25 @@ public class PopupCameraService extends Service {
 
     private void registerReceiver() {
         IntentFilter filter = new IntentFilter();
-        filter.addAction("android.intent.action.ACTION_SHUTDOWN");
-        filter.addAction("android.intent.action.SCREEN_ON");
-        filter.addAction("android.intent.action.SCREEN_OFF");
-        filter.addAction("lineageos.intent.action.CAMERA_STATUS_CHANGED");
-        filter.addAction("lineageos.intent.action.ACTIVE_PACKAGE_CHANGED");
-        this.registerReceiver(mIntentReceiver, filter);
+        filter.addAction(Intent.ACTION_SCREEN_OFF);
+        filter.addAction(lineageos.content.Intent.ACTION_CAMERA_STATUS_CHANGED);
+        registerReceiver(mIntentReceiver, filter);
     }
-
-    private BroadcastReceiver mIntentReceiver = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            final String action = intent.getAction();
-            if (lineageos.content.Intent.ACTION_CAMERA_STATUS_CHANGED.equals(action)) {
-               mCameraState = intent.getExtras().getString(lineageos.content.Intent.EXTRA_CAMERA_STATE);
-               updateMotor(mCameraState);
-            }
-        }
-    };
 
     private void updateMotor(String cameraState) {
         if (mMotor == null) return;
         try {
-           if (cameraState.equals(openCameraState) && mMotor.getMotorStatus() == 13) {
-               lightUp();
-               mMotor.popupMotor(1);
-               mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor, SensorManager.SENSOR_DELAY_NORMAL);
-           } else if (cameraState.equals(closeCameraState) && mMotor.getMotorStatus() == 11) {
-               lightUp();
-               mMotor.takebackMotor(1);
-               mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
-           }
-        } catch(Exception e) {
+            if (cameraState.equals(openCameraState) && mMotor.getMotorStatus() == 13) {
+                lightUp();
+                mMotor.popupMotor(1);
+                mSensorManager.registerListener(mFreeFallListener, mFreeFallSensor, SensorManager.SENSOR_DELAY_NORMAL);
+            } else if (cameraState.equals(closeCameraState) && mMotor.getMotorStatus() == 11) {
+                lightUp();
+                mMotor.takebackMotor(1);
+                mSensorManager.unregisterListener(mFreeFallListener, mFreeFallSensor);
+            }
+        } catch (Exception e) {
+            // Do nothing
         }
     }
 
@@ -133,20 +137,6 @@ public class PopupCameraService extends Service {
             }
         }, 1200);
     }
-
-    private SensorEventListener mFreeFallListener = new SensorEventListener() {
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            if (event.sensor.getType() == FREE_FALL_SENSOR_ID && event.values[0] == 2.0f) {
-               updateMotor(closeCameraState);
-               goBackHome();
-            }
-        }
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
-        }
-    };
 
     public void goBackHome() {
         Intent homeIntent = new Intent(Intent.ACTION_MAIN);
