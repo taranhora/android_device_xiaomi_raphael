4250702d9772ba8ddd79fbdef27080bb57f418b1
taran107 <hora.cooltaran@gmail.com>
2020-07-18 01:15:38 +0530

    Blissify

--- bliss_raphael.mk
+++ bliss_raphael.mk
@@ -15,16 +15,15 @@
 # Inherit from those products. Most specific first.
 $(call inherit-product, device/xiaomi/raphael/msmnile.mk)
 
-# Inherit some common Colt stuff.
-$(call inherit-product, vendor/colt/config/common_full_phone.mk)
+# Inherit some common Bliss stuff.
+$(call inherit-product, vendor/bliss/config/common_full_phone.mk)
 
 # Inherit from ANXCamera vendor.
 $(call inherit-product, vendor/ANXCamera/config.mk)
 
-TARGET_GAPPS_ARCH := arm64
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := colt_raphael
+PRODUCT_NAME := bliss_raphael
 PRODUCT_DEVICE := raphael
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := MI 9T Pro
@@ -36,8 +35,5 @@ PRODUCT_BUILD_PROP_OVERRIDES += \
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
 
-# ColtOS stuff
-TARGET_BOOT_ANIMATION_RES := 1080x2340
-COLT_BUILD_TYPE := Official
-COLT_DEVICE_MAINTAINER := HSgod
-EXTRA_FOD_ANIMATIONS := true
+# BOOT Animation
+TARGET_BOOT_ANIMATION_RES := 1440
